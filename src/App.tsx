0c6dd9d6f4559be3245ca1a1c5fddb109f7dd981
Martin <macamp34@gmail.com>
2023-04-18 16:42:43 +0200

    Add linter & eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { db } from './firebase'
 
 function App() {
-    console.log(db);
     const [count, setCount] = useState(0)
-
-
 
     return (
         <div className="App">
