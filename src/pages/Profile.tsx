2c5eaf0a19f5c23f4e05441f47bd40fe862c28d6
Martin <macamp34@gmail.com>
2023-05-06 21:42:06 +0200

    Add logout to profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,12 @@
 import { Box } from '@mui/material';
 
-const ProfilePage = () => <Box sx={{ padding: 2 }}>Profile</Box>;
+import { signOut } from '../firebase';
+import TextButton from '../components/TextButton';
+
+const ProfilePage = () => (
+	<Box sx={{ padding: 2 }}>
+		<TextButton onClick={signOut}>Sign out</TextButton>
+	</Box>
+);
 
 export default ProfilePage;
