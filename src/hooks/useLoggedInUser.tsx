a4eb15131036264d0f98077a19db432103156d4c
Martin <macamp34@gmail.com>
2023-05-06 19:52:12 +0200

    Adjust logged in user to return undefined/null in separate cases

--- src/hooks/useLoggedInUser.tsx
+++ src/hooks/useLoggedInUser.tsx
@@ -6,11 +6,11 @@ import { onAuthChanged } from '../firebase';
 // Hook providing logged in user information
 const useLoggedInUser = () => {
 	// Hold user info in state
-	const [user, setUser] = useState<User>();
+	const [user, setUser] = useState<User | null>();
 
 	// Setup onAuthChanged once when component is mounted
 	useEffect(() => {
-		onAuthChanged(u => setUser(u ?? undefined));
+		onAuthChanged(u => setUser(u));
 	}, []);
 
 	return user;
