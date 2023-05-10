ee85e3c1c4b59b7dd296efe8b7cb89800751756d
Martin <macamp34@gmail.com>
2023-05-10 22:24:21 +0200

    Add songs to leaderboard with new hook

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,18 +1,48 @@
 import { Box, Typography } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 
-import useSongs from '../hooks/useSongs';
 import SongPanel from '../components/SongPanel';
+import { Song } from '../firebase';
+import useSongsWithRatings from '../hooks/useSongsWithRatings';
 
 const LeaderboardPage = () => {
-	const songs = useSongs();
+	const [currentlyPlaing, setCurrentlyPlaying] = useState<Song | null>(null);
+	const audio = useRef<HTMLAudioElement>(null);
+	const songs = useSongsWithRatings();
+
+	useEffect(() => {
+		if (currentlyPlaing) audio.current?.play();
+	}, [currentlyPlaing]);
+
+	const sortedSongs = [...songs].sort(
+		(songA, songB) => songB.ratings.true - songA.ratings.true
+	);
 
 	return (
 		<>
 			<Typography variant="h5" sx={{ fontWeight: 'bold' }} mb={2}>
 				Leaderboard
 			</Typography>
-			{songs?.map((song, index) => (
-				<SongPanel song={song} key={index} sx={{ mb: 2 }} />
+
+			{currentlyPlaing && (
+				<Box
+					component="audio"
+					display="none"
+					ref={audio}
+					src={currentlyPlaing.preview}
+				/>
+			)}
+
+			{sortedSongs.map((song, index) => (
+				<SongPanel
+					song={song}
+					key={index}
+					sx={{ mb: index === songs.length - 1 ? 15 : 2 }}
+					rating={` ${song.ratings.true} | ${song.ratings.false}`}
+					isPlaying={song === currentlyPlaing}
+					onStop={() => setCurrentlyPlaying(null)}
+					onPlay={song => setCurrentlyPlaying(song)}
+				/>
 			))}
 		</>
 	);
